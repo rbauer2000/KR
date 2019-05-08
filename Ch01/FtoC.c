d199673d5e37d2129580eb4542125bbc94c27693
Russell <bauer.russell@gmail.com>
2019-05-08 15:43:57 -0600

    Final version of FtoC

--- Ch01/FtoC.c
+++ Ch01/FtoC.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
+#define LOWER 0        /* lower limit of temerature table */
+#define UPPER 300      /* upper limit */
+#define STEP  20       /* step size */
+
+
+
 /* print Fahrenheit-Celsius table for F = 0, 20, ..., 300 */
 
 int main()
 {
 	int fahr, celsius;
-	int lower, upper, step;
 
-	lower = 0; /* lower limit of temerature table */
-	upper = 300; /* upper limit */
-        step = 20; /* step size */
-
-	fahr = lower;
-	while (fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
-		fahr = fahr + step;
-	}
+        for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32)); 
 	return(0);
 }
